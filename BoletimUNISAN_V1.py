3ee943fac0fb45b982f4d4cd36772bf2aa9f986f
yangzkiller <clisesque2k18@gmail.com>
2022-10-12 16:20:22 -0300

    base nova

--- BoletimUNISAN_V1.py
+++ BoletimUNISAN_V1.py
@@ -6,10 +6,28 @@
 
 #BIBLIOTECA DE CONTROLE DE TEMPO
 from time import sleep
-#LAÇO
-while True:
-    #DISPLAY
-    print(yellow, "-=" * 20)
-    print (blue, "{:^40}".format("Boletim Escolar"))
-    print(yellow, "-=" * 20 ,blue)
-    sleep(1)
+
+#DISPLAY
+print("-=" * 20)
+print ("{:^40}".format("Boletim Escolar"))
+print("-=" * 20)
+sleep(1)
+
+ #ENTRADA DE DADOS
+P1 = float(input("Digite a nota da  1° Prova: "))
+P2 = float(input("Digite a nota do  2° Prova: "))
+
+#PROCESSAMENTO
+s = P1 * 1 + P2 * 2
+media = s / 3
+    
+#ESTRUTURAIS CONDICIONAIS
+print("--" * 20)
+if media >= 6 :
+    print(f"Aluno (APROVADO) com média: {media:.2f}")
+    print("--" * 20)
+    sleep(2)
+else:
+    print(f"Aluno (REPROVADO) com média: {media:.2f}")   
+    print("--" * 20)
+    sleep(2)
