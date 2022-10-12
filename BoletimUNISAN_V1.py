3358ca96b9871c51f2e24324d59c09b7fe7cbdbf
yangzkiller <clisesque2k18@gmail.com>
2022-10-12 19:27:10 -0300

    Adição de um loop e menu

--- BoletimUNISAN_V1.py
+++ BoletimUNISAN_V1.py
@@ -13,21 +13,57 @@ print ("{:^40}".format("Boletim Escolar"))
 print("-=" * 20)
 sleep(1)
 
- #ENTRADA DE DADOS
-P1 = float(input("Digite a nota da  1° Prova: "))
-P2 = float(input("Digite a nota do  2° Prova: "))
+#LAÇO
+sair = False
+while not sair:
+    #MENU
+    sleep(1)
+    print ("{:^40}".format("||   MENU   ||"))
+    print ("--" * 20)
+    print("[1] Calcular média das notas \n[2] Sair")
+    print(" ")
+    usuario = int(input("Digite uma opção: "))
+    print("==" *20)
 
-#PROCESSAMENTO
-s = P1 * 1 + P2 * 2
-media = s / 3
+    #ESTRUTURAIS CONDICIONAIS MENU
+    #OPCAO - 1
+    if usuario == 1:
+        #ENTRADA DE DADOS
+        P1 = float(input("Digite a nota da  1° Prova: "))
+        P2 = float(input("Digite a nota do  2° Prova: "))
+
+        #PROCESSAMENTO
+        s = P1 * 1 + P2 * 2
+        media = s / 3
     
-#ESTRUTURAIS CONDICIONAIS
-print("--" * 20)
-if media >= 6 :
-    print(f"Aluno (APROVADO) com média: {media:.2f}")
-    print("--" * 20)
-    sleep(2)
-else:
-    print(f"Aluno (REPROVADO) com média: {media:.2f}")   
-    print("--" * 20)
-    sleep(2)
+        #ESTRUTURA CONDICIONAL
+        print("--" * 20)
+        #APROVADO
+        if media >= 6 :
+            print(f"Aluno (APROVADO) com média: {media:.2f}")
+            print("==" * 20)
+            sleep(1)
+        #REPROVADO
+        else:
+            print(f"Aluno (REPROVADO) com média: {media:.2f}")   
+            print("--" * 20)
+            sleep(1)
+
+    #OPCAO - 2
+    elif usuario == 2:
+        sleep(1)
+        print("{:^40}".format("-- Programa Encerrado --"))
+        print("==" * 20)
+        sleep(1)
+        sair = True
+    
+    #ERRO
+    else:
+        sleep(1)
+        print("{:^40}".format("-- ERRO!!! Opção Inválida  --"))
+        print("==" * 20)
+
+
+
+
+
