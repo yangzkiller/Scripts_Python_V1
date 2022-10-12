efc0f906a8d581031b5b06d0a9827c443dfcdffe
Yangz <113180167+yangzkiller@users.noreply.github.com>
2022-10-12 14:38:27 -0300

    Update BoletimUNISAN_V1.py

--- BoletimUNISAN_V1.py
+++ BoletimUNISAN_V1.py
@@ -3,3 +3,13 @@
 
 #VERSÃO - V1 - Boletim escolar com nota de 2 bimestres, sendo a segunda nota tendo peso dois.
 #DATA: 12/05/22
+
+#BIBLIOTECA DE CONTROLE DE TEMPO
+from time import sleep
+#LAÇO
+while True:
+    #DISPLAY
+    print(yellow, "-=" * 20)
+    print (blue, "{:^40}".format("Boletim Escolar"))
+    print(yellow, "-=" * 20 ,blue)
+    sleep(1)
