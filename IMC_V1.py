24deb7bf7a419a3333e89441e54d435aedd3db6a
yangzkiller <clisesque2k18@gmail.com>
2022-10-13 03:44:43 -0300

    modificações em andamento

--- IMC_V1.py
+++ IMC_V1.py
@@ -4,14 +4,36 @@
 
 #V1 - Programa que calcula o IMC de uma pessoa
 
-#VARIAVEIS
-print('-=' *20)
-peso =  float(input('Qual é seu peso? (Kg) '))
-altura = float(input('Qual é sua altura? (m) '))
-print('-=' *20)
-imc = peso / (altura ** 2)
+#CABEÇALHO
+def cabecalho():
+    print("-=" *20)
+    print("{:^40}".format("CALCULADORA IMC"))
+    print('-=' *20)
 
-#ESTRUTURA CONDICIONAL
+#MENU
+def menu():
+    print ("{:^40}".format("||   MENU   ||"))
+    print ("--" * 20)
+    print("[1] Calcular IMC \n[2] Mostrar tabela de referencia \n[3] Sair")
+    print(" ")
+
+#FORMULÁRIO
+def formularioEntrada(peso,altura ):
+    #DISPLAY
+    print('-=' *20)
+    print("{:^40}".format("||   INFORME   ||"))
+    print('--' *20)
+    #ENTRADA DE DADOS
+    nome = (input("--> Nome: "))
+    sexo = (input("--> Sexo: "))
+    idade = (input("--> Idade: "))       
+    peso =  float(input('Qual é seu peso (Kg): '))
+    altura = float(input('Qual é sua altura (m): '))
+    dados = nome, sexo, idade
+    calc = peso / (altura ** 2)
+    return calc
+
+'''#ESTRUTURA CONDICIONAL
 print('O IMC desta pessoa é de {:.1f}'.format(imc))
 if imc < 18.5:
     print('Voce está ABAIXO DO PESO normal')
@@ -25,3 +47,29 @@ elif 35 <= imc < 40:
     print('Voce está com OBESIDADE GRAU II (severa)')
 else:
     print('Voce está com OBESIDADE GRAU III (mórbida)')
+'''
+#EXECUÇÃO
+cabecalho()
+
+#LAÇO
+sair = False
+while not sair:
+    menu()
+    #ENTRADA DE DADOS
+    usuario = int(input("Digite uma opção: "))
+
+    #ESTRUTURA CONDICIONAL - MENU
+    if usuario == 1:
+        
+       #formulario
+        imc = (formularioEntrada(peso,altura))
+        print (imc)
+
+    elif usuario == 2:
+        print("2")
+    
+    elif usuario == 3:
+        sair = True
+
+    else:
+        print("erro")
