442b3a3308b95aaa83404e9733895c68b1a0c26c
yangzkiller <clisesque2k18@gmail.com>
2022-10-18 16:34:06 -0300

    Add Color

--- MediaNotas.py
+++ MediaNotas.py
@@ -7,22 +7,29 @@
 #BIBLIOTECA DE CONTROLE DE TEMPO
 from time import sleep
 
+#TERMINAL COLOR
+reset = '\033[37m'
+red = '\033[91m'
+green = '\033[92m'
+yellow = '\033[93m'
+blue = '\033[94m'
+
 #CABECALHO
-print("-=" * 20)
-print ("{:^40}".format("Boletim Escolar"))
-print("-=" * 20)
+print(red,"-=" * 20)
+print (yellow,"{:^40}".format("Boletim Escolar"))
+print(red,"-=" * 20)
 sleep(1)
 
 #LAÇO
 sair = False
 while not sair:
     #MENU
-    print ("{:^40}".format("||   MENU   ||"))
-    print ("--" * 20)
-    print("[1] Calcular média de 2 notas\n[2] Calcular média de 4 notas\n[3]Sair")
+    print (yellow,"{:^40}".format("||   MENU   ||"))
+    print (red,"--" * 20)
+    print(green," [1]",reset,"Calcular média de 2 notas\n",green,"[2]",reset,"Calcular média de 4 notas\n",red,"[3]",reset,"Sair",yellow)
     print(" ")
-    usuario = int(input("Digite uma opção: "))
-    print("==" *20)
+    usuario = int(input(" Digite uma opção: "))
+    print(red,"==" *20,reset)
 
     #ESTRUTURAIS CONDICIONAIS - MENU
     #OPCAO - 1
@@ -30,7 +37,7 @@ while not sair:
         #ENTRADA DE DADOS
         nota_1 = float(input("Digite a nota da  1° Prova: "))
         nota_2 = float(input("Digite a nota do  2° Prova: "))
-        print("--" * 20)
+        print(red,"--" * 20)
 
         #PROCESSAMENTO
         s = nota_1 + nota_2 
@@ -40,13 +47,13 @@ while not sair:
 
         #APROVADO
         if media >= 6 :
-            print(f"Aluno (APROVADO) com média: {media:.2f}")
-            print("==" * 20)
+            print(yellow,"Aluno",green,"(APROVADO)",yellow,"com média:",green,f"{media:.2f}")
+            print(red,"==" * 20)
             sleep(1)
         #REPROVADO
         else:
-            print(f"Aluno (REPROVADO) com média: {media:.2f}")   
-            print("--" * 20)
+            print(yellow,"Aluno",red,"(REPROVADO)",yellow,"com média:",red,f"{media:.2f}")   
+            print(red,"--" * 20,reset)
             sleep(1)
         
     #OPCAO - 2
@@ -56,7 +63,7 @@ while not sair:
         nota_2 = float(input("Digite a nota do  2° Prova: "))
         nota_3 = float(input("Digite a nota da  3° Prova: "))
         nota_4 = float(input("Digite a nota do  4° Prova: "))
-        print("--" * 20)
+        print(red,"--" * 20)
 
         #PROCESSAMENTO
         s = nota_1 + nota_2 + nota_3 + nota_4
@@ -66,26 +73,26 @@ while not sair:
 
         #APROVADO
         if media >= 6 :
-            print(f"Aluno (APROVADO) com média: {media:.2f}")
-            print("==" * 20)
+            print(yellow,"Aluno",green,"(APROVADO)",yellow,"com média:",green,f"{media:.2f}")
+            print(red,"==" * 20)
             sleep(1)
         #REPROVADO
         else:
-            print(f"Aluno (REPROVADO) com média: {media:.2f}")   
-            print("--" * 20)
+            print(yellow,"Aluno",red,"(REPROVADO)",yellow,"com média:",red,f"{media:.2f}")   
+            print(red,"--" * 20)
             sleep(1)
         
-
     #OPCAO - 3
     elif usuario == 3:
         sleep(1)
-        print("{:^40}".format("-- Programa Encerrado --"))
-        print("==" * 20)
+        print(yellow,"{:^40}".format("-- Programa Encerrado --"))
+        print(red,"==" * 20,reset)
         sleep(1)
         sair = True
 
     #ERRO
     else:
         sleep(1)
-        print("{:^40}".format("-- ERRO!!! Opção Inválida  --"))
-        print("==" * 20)
+        print(red,"{:^40}".format("-- ERRO!!! Opção Inválida  --"))
+        print(red,"==" * 20)
+
