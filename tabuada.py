6aeadce145f4f6402414b0335e02bdf0eab67117
yangzkiller <clisesque2k18@gmail.com>
2022-10-31 19:55:38 -0300

    Add color

--- tabuada.py
+++ tabuada.py
@@ -6,30 +6,37 @@
 #BIBLIOTECA DE CONTROLE DE TEMPO
 from time import sleep
 
+#TERMINAL COLOR
+reset = '\033[37m'
+red = '\033[91m'
+green = '\033[92m'
+yellow = '\033[93m'
+blue = '\033[94m'
+
 #LIMPAR
 def limpar():
     print("\n" * 100)
 
 #CABEÇALHO
 def cabecalho():
-    print("-=" *20)
-    print("{:^40}".format("TABUADA"))
-    print('-=' *20)
+    print(red,"-=" *20)
+    print(yellow,"{:^40}".format("TABUADA"))
+    print(red,'-=' *20,yellow)
 
 #TABUADA
 def tabuada():
-    #LAÇO Principal
+    #LAÇO PRINCIPAL
     while True:
         #ENTRADA DE DADOS
         n = int(input('Quer ver a tabuada de qual valor: '))
-        print('=-' *20)
+        print(red,'=-' *20)
         #ESTRUTURA BREAK
-        if n < 0:
+        if n < 1:
             break
         #CONTADOR
         for c in range(1, 11):
-            print(f'{n} x {c} = {n*c}') 
-        print('=-' *20)   
+            print(yellow, f'{n} x {c} = ',green,f'{n*c}') 
+        print(red,'=-' *20,yellow)   
     print('PROGRAMA TABUADA ENCERRADO')
 
 #EXECUÇÃO
