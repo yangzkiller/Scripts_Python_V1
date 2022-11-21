fd56f19634c26729ce7f7f7c32d59643418c6c4e
yangzkiller <clisesque2k18@gmail.com>
2022-11-21 02:11:21 -0300

    Terminal color

--- Ano_bissexto.py
+++ Ano_bissexto.py
@@ -2,10 +2,18 @@
 #PROGRAMADOR: YANGZ
 #DATA: 20/11/22
 
+#CORES TERMINAL
+reset = '\033[1;37m'
+red = '\033[1;31m'
+green = '\033[1;32m'
+blue = '\033[1;34m'
+purple = '\033[1;35m'
+yellow = '\033[1;33m'
+
 #DISPLAY
-print("=-" *20)
+print(red,"=-" *20,reset)
 print("  --> CALCULADORA DE ANO BISSEXTO <--")
-print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print(red,"=-" *20,reset)
 
 #LAÇO
 sair = True
@@ -13,35 +21,36 @@ while (sair == True):
 
     #MENU
     print("             ||   MENU   ||")
-    print("--" *20)
-    print("[1] Calculo Bissexto\n[2] Sair")
+    print(red,"--" *20,yellow)
+    print("  [1] Calculo Bissexto\n",red,"[2] Sair",reset)
     print(" ")
     #ENTRADA DE DADOS
     opcao = int(input("Digite uma opção: "))
-    print("=-" *20)
+    print(red,"=-" *20,reset)
                         
     #ESTRUTURA CONDICIONAL - MENU
     #OPÇÃO - 1
     if (opcao == 1):
     #ENTRADA DE DADOS
         ano = int(input("Digite o Ano: "))
-        print("--" *20)
+        print(red,"--" *20)
 
         #PROCESSAMENTO DE DADOS
         if(ano % 4 == 0):
-            print("--> Bissexto <--")
-            print("=-" *20)     
+            print(green,"--> Bissexto <--",reset)
+            print(red,"=-" *20,reset)     
         else:
-            print("--> Não é Bissexto <--")
-            print("=-" *20)
+            print(red,"--> Não é Bissexto <--")
+            print("=-" *20,reset)
                 
     #OPÇÃO - 2
     elif (opcao == 2):
-        print("      --> PROGRAMA ENCERRADO <--")
-        print("=-" *20)
+        print(red,"      --> PROGRAMA ENCERRADO <--")
+        print("=-" *20,reset)
         sair = False
             
     #ERRO
     else:
-        print("        !ERRO! Entrada Inválida")
-        print("=-" *20)
+        print(red,"        !ERRO! Entrada Inválida")
+        print("=-" *20,reset)
+
